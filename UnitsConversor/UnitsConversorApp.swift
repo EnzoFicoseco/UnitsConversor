9b50263102b4182eb5f0e052fd67ab6b8be4eca6
EnzoFicoseco <enzoficoseco2704@gmail.com>
2024-07-15 14:57:14 -0300

    Initial Commit

---
+++ UnitsConversor/UnitsConversorApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitsConversorApp.swift
+//  UnitsConversor
+//
+//  Created by Enzo Ficoseco on 15/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct UnitsConversorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
